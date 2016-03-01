f4277f029491aab2e52a440331048c6fe1d787dd
jawittdesigns <Killjoy11>
2016-03-01 15:34:54 -0500

    Fixed Homepage

--- template-parts/entry/page.php
+++ template-parts/entry/page.php
@@ -11,6 +11,43 @@ endif;
 wp_reset_postdata();
 ?>
 <div class="content__wrapper body-text">
+  <?php if( is_home() || is_front_page() ) : ?>
+  <section class="intro">    
+    <?php if( $rxoc_page_intro_heading ) : ?>   
+      <h2 class="intro__heading">   
+        <?php echo $rxoc_page_intro_heading; ?>   
+      </h2>   
+    <?php endif; ?>   
+    <?php   
+    if( $rxoc_page_intro_text ) :   
+      if ( has_post_thumbnail() ) :   
+    ?>    
+      <div class="row">   
+        <div class="col__2-3 intro__left">    
+          <?php echo $rxoc_page_intro_text; ?>    
+        </div>    
+        <div class="col__1-3 intro__right">   
+          <?php the_post_thumbnail();?>   
+        </div>    
+      </div>    
+    <?php   
+      else :    
+        echo $rxoc_page_intro_text;   
+      endif;    
+    endif;    
+    ?>    
+  </section>
+  <section class="content">
+    <?php if( $rxoc_content_heading ) : ?>
+      <h3 class="content__heading">
+        <?php echo $rxoc_content_heading; ?>
+      </h3>
+    <?php
+    endif;
+    the_content(); 
+    ?>
+  </section>
+  <?php else : ?>
   <section class="content">
     <?php if( $rxoc_content_heading ) : ?>
       <h2 class="content__heading">
@@ -21,4 +58,5 @@ wp_reset_postdata();
     the_content(); 
     ?>
   </section>
+<?php endif; ?>
 </div>
