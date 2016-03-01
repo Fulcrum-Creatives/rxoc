62d994cddb1160a3f4f572200d471dd2e76ab0ce
jawittdesigns <Killjoy11>
2016-03-01 15:24:20 -0500

    CSS and fixed admin functionality

--- template-parts/entry/page.php
+++ template-parts/entry/page.php
@@ -11,36 +11,11 @@ endif;
 wp_reset_postdata();
 ?>
 <div class="content__wrapper body-text">
-  <section class="intro">
-    <?php if( $rxoc_page_intro_heading ) : ?>
-      <h2 class="intro__heading">
-        <?php echo $rxoc_page_intro_heading; ?>
-      </h2>
-    <?php endif; ?>
-    <?php
-    if( $rxoc_page_intro_text ) :
-      if ( has_post_thumbnail() ) :
-    ?>
-      <div class="row">
-        <div class="col__2-3 intro__left">
-          <?php echo $rxoc_page_intro_text; ?>
-        </div>
-        <div class="col__1-3 intro__right">
-          <?php the_post_thumbnail();?>
-        </div>
-      </div>
-    <?php
-      else :
-        echo $rxoc_page_intro_text;
-      endif;
-    endif;
-    ?>
-  </section>
   <section class="content">
     <?php if( $rxoc_content_heading ) : ?>
-      <h3 class="content__heading">
+      <h2 class="content__heading">
         <?php echo $rxoc_content_heading; ?>
-      </h3>
+      </h2>
     <?php
     endif;
     the_content(); 
